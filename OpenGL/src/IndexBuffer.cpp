6d4d703ae277147180a5b9317eef97aec174d5e0
Hjalmar Basile <hjalmar.basile@gmail.com>
2018-09-19 21:12:48 +0200

    Add IndexBuffer class

---
+++ OpenGL/src/IndexBuffer.cpp
@@ -0,0 +1,25 @@
+#include "IndexBuffer.h"
+
+#include "Renderer.h"
+
+IndexBuffer::IndexBuffer(const unsigned int* data, unsigned int count) : m_Count(count)
+{
+	GLCheckErrorCall(glGenBuffers(1, &m_RendererID));
+	GLCheckErrorCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID));
+	GLCheckErrorCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(unsigned int), data, GL_STATIC_DRAW));
+}
+
+IndexBuffer::~IndexBuffer()
+{
+	GLCheckErrorCall(glDeleteBuffers(1, &m_RendererID));
+}
+
+void IndexBuffer::Bind() const
+{
+	GLCheckErrorCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID));
+}
+
+void IndexBuffer::Unbind() const
+{
+	GLCheckErrorCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+}
