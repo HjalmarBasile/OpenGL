36d6aeb0ce3fc2d0c18aaf00fae79fd5489abfbc
Hjalmar Basile <hjalmar.basile@gmail.com>
2018-09-19 20:41:36 +0200

    Add VertexBuffer class

---
+++ OpenGL/src/VertexBuffer.cpp
@@ -0,0 +1,28 @@
+#include "VertexBuffer.h"
+
+#include "Renderer.h"
+
+VertexBuffer::VertexBuffer(const void * data, size_t size)
+{
+	/* Generate Vertex Buffer with modern OpenGL */
+	GLCheckErrorCall(glGenBuffers(1, &m_RendererID));
+	GLCheckErrorCall(glBindBuffer(GL_ARRAY_BUFFER, m_RendererID));
+
+	/* Pass the data and specify that it must be drawn */
+	GLCheckErrorCall(glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW));
+}
+
+VertexBuffer::~VertexBuffer()
+{
+	GLCheckErrorCall(glDeleteBuffers(1, &m_RendererID));
+}
+
+void VertexBuffer::Bind()
+{
+	GLCheckErrorCall(glBindBuffer(GL_ARRAY_BUFFER, m_RendererID));
+}
+
+void VertexBuffer::Unbind()
+{
+	GLCheckErrorCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+}
