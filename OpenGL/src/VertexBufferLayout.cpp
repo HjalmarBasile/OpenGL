8c64f9796a20e35fe5fdb840f1aba7ed55b233d2
Hjalmar Basile <hjalmar.basile@gmail.com>
2018-09-21 22:55:47 +0200

    Move VertexBufferLayout template methods in header file

--- OpenGL/src/VertexBufferLayout.cpp
+++ OpenGL/src/VertexBufferLayout.cpp
@@ -1,33 +1,3 @@
 #include "VertexBufferLayout.h"
 
 VertexBufferLayout::VertexBufferLayout() : m_Stride(0) {}
-
-template<typename T>
-void VertexBufferLayout::Push(GLint count)
-{
-	static_assert(false, "Appropriate template specialization not found for VertexBufferLayout::Push");
-}
-
-template<>
-void VertexBufferLayout::Push<unsigned char>(GLint count)
-{
-	size_t size = count * sizeof(GLubyte);
-	m_Elements.push_back({ count, GL_UNSIGNED_BYTE, GL_TRUE, size});
-	m_Stride += size;
-}
-
-template<>
-void VertexBufferLayout::Push<unsigned int>(GLint count)
-{
-	size_t size = count * sizeof(GLuint);
-	m_Elements.push_back({ count, GL_UNSIGNED_INT, GL_FALSE, size });
-	m_Stride += size;
-}
-
-template<>
-void VertexBufferLayout::Push<float>(GLint count)
-{
-	size_t size = count * sizeof(GLfloat);
-	m_Elements.push_back({ count, GL_FLOAT, GL_FALSE, size });
-	m_Stride += size;
-}
--- OpenGL/src/VertexBufferLayout.h
+++ OpenGL/src/VertexBufferLayout.h
@@ -20,10 +20,35 @@ private:
 public:
 	VertexBufferLayout();
 
-	template<typename T> void Push(GLint count);
-	template<>			 void Push<unsigned char>(GLint count);
-	template<>			 void Push<unsigned int>(GLint count);
-	template<>			 void Push<float>(GLint count);
+	template<typename T>
+	void Push(GLint count)
+	{
+		static_assert(false, "Appropriate template specialization not found for VertexBufferLayout::Push");
+	}
+
+	template<>
+	void Push<unsigned char>(GLint count)
+	{
+		size_t size = count * sizeof(GLubyte);
+		m_Elements.push_back({ count, GL_UNSIGNED_BYTE, GL_TRUE, size });
+		m_Stride += size;
+	}
+
+	template<>
+	void Push<unsigned int>(GLint count)
+	{
+		size_t size = count * sizeof(GLuint);
+		m_Elements.push_back({ count, GL_UNSIGNED_INT, GL_FALSE, size });
+		m_Stride += size;
+	}
+
+	template<>
+	void Push<float>(GLint count)
+	{
+		size_t size = count * sizeof(GLfloat);
+		m_Elements.push_back({ count, GL_FLOAT, GL_FALSE, size });
+		m_Stride += size;
+	}
 
 	inline GLsizei GetStride() const { return m_Stride; }
 	inline const std::vector<VertexBufferElement>& GetElements() const { return m_Elements; }
